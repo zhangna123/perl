3940b4109070e862a5ff1ef41874ad91d7babeb1
zhangna123 <1107034934@qq.com>
2016-12-01 09:18:03 +0800

    init this is ..

--- class5/exercise5_6.pl
+++ class5/exercise5_6.pl
@@ -1,6 +1,23 @@
 #!/usr/bin/perl -w
-print "please enter your first DNA: "
+print "please enter your first DNA: ";
 $DNA1 = <STDIN>;
-print "please enter your second DNA: "
+chomp $DNA1;
+print "please enter your second DNA: ";
 $DNA2 = <STDIN>;
-
+chomp $DNA2;
+$DNA1 =~ tr/ACGTacgt/TGCATGCA/;
+$DNA2 =~ tr/acgt/ACGT/;
+@DNA1 = split ( '',$DNA1 );
+@DNA2 = split ( '',$DNA2 );
+for ( $position = 0; $position < length $DNA1; ++$position)
+   {
+    $x = pop @DNA1;
+    $y = shift @DNA2;
+    if ( $x eq $y ) { next;}
+    else {
+      print "they are not recom!\n";
+      exit;
+   } 
+   }
+print "they are recom!\n.";
+exit;
