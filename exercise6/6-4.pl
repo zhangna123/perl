6d1939f8aabfe16f2cefdb2cc143cce85d67b448
zhangna123 <1107034934@qq.com>
2016-12-08 15:05:27 +0800

    init ccc

--- exercise6/6-4.pl
+++ exercise6/6-4.pl
@@ -1,2 +1,11 @@
 #!/usr/bin/perl -w
-
+use strict;
+my $help = "$0 :DNA\n\n";
+my $man = "$0 :this is a mannul.\n\n";
+if ($ARGV[0] eq '-h'  || $ARGV[0] eq '-help')  {
+     print $help;
+   }
+elsif ($ARGV[0] eq '-m'  || $ARGV[0] eq '-man')  {
+     print $man;
+   }
+exit;
