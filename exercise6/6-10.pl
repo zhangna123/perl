c1a34e743c99fd12f080081d2d834ee2635666a7
zhangna123 <110703493402qq.com>
2016-12-08 09:24:35 +0800

    init

--- exercise6/6-10.pl
+++ exercise6/6-10.pl
@@ -2,20 +2,35 @@
 use strict;
 print "please enter the filename: ";
 my $filename = <STDIN>;
-
-
-sub modif {
 chomp $filename;
 unless ( open (DNA, $filename) ) {
      print " cannot open file: \"$filename\"\n\n";
      exit;
     }
-@dna = <DNA>;
+my @dna = <DNA>;
 close DNA;
-$dna = join ('',@dna);
-$dna =~ s/\s//g;
+my $result = '';
+$result = mo(\@dna);
+exit;
 
-
+sub mo {
+my ($dna) = @_;
+print "this is your text :\n @$dna\n";
+my $dna1 = join ('', @$dna);
+$dna1 =~ s/\s//g;
+print "please enter your target:";
+my $target = <STDIN>;
+chomp $target;
+print "please enter your new string:";
+my $newstring = <STDIN>;
+chomp $newstring;
+$dna1 =~ s/$target/$newstring/g;
+my $fh_out = $filename;
+open my $FH_OUT,'>', $fh_out or die "$0:failed to open output file '$fh_out': $!\n";
+select $FH_OUT;
+print "$dna1\n";
+close $FH_OUT or warn "$0:failed\n";
+return $dna1;
 }
 
 
