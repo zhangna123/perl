435457d11f366eded6bfa2527d458d324e212b32
zhangna123 <1107034934@qq.com>
2016-12-18 22:15:11 +0800

    Update BeginPerlBioinfo.pm

--- BeginPerlBioinfo.pm
+++ BeginPerlBioinfo.pm
@@ -1,4 +1,17 @@
 
+sub dna2peptide {
+  my($dna) = @_;
+  use strict;
+  use warnings;
+  use BeginPerlBioinfo;     # see Chapter 6 about this module
+  # Initialize variables
+  my $protein = '';
+  # Translate each three-base codon to an amino acid, and append to a protein 
+  for(my $i=0; $i < (length($dna) - 2) ; $i += 3) {
+    $protein .= codon2aa( substr($dna,$i,3));
+  }
+  return $protein;
+}
 
 
 
