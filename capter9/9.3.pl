2e8590b8100223bb9dfa5dfd28fe5ee80ebc7d06
zhangna123 <1107034934@qq.com>
2016-12-25 14:57:06 +0800

    Update 9.3.pl

---
+++ capter9/9.3.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+use BeginPerlBioinfo;    
+my %rebase_hash      = ();
+my @file_data        = ();
+my $query            = '';
+my $dna              = '';
+my $recognition_site = '';
+my $regexp           = '';
+my @locations        = ();
+
+@file_data = get_file_data("sample.dna");
+
+$dna = extract_sequence_from_fasta_data(@file_data);
+
+%rebase_hash = parseREBASE('bionet');
+
+do {
+    print "Search for what restriction site for (or quit)?: ";
+    $query = <STDIN>;
+    chomp $query;
+    # Exit if empty query
+    if ( $query =~ /^\s*$/ ) {
+        exit;
+    }
+
+    if ( exists $rebase_hash{$query} ) {
+        ( $recognition_site, $regexp ) = split( " ", $rebase_hash{$query} );
+        # Create the restriction map
+        @locations = match_positions( $regexp, $dna );
+
+        if (@locations) {
+            print "Searching for $query $recognition_site $regexp\n";
+            print "A restriction site for $query at locations:\n";
+            print join( " ", @locations ), "\n";
+        }
+        else {
+            print "A restriction enzyme $query is not in the DNA:\n";
+        }
+    }
+    print "\n";
+} until ( $query =~ /quit/ );
+exit;
